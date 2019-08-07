0de9dea2ba6c6ffcc378875674ca00568593fca1
Selvaganesh <selvaganeshrajam@gmail.com>
2019-08-07 11:06:14 +0530

    Script

--- selenium-node/index.js
+++ selenium-node/index.js
@@ -1,9 +1,25 @@
-const { Builder, By, Key, until } = require('selenium-webdriver')
+const { Builder, By, Key, until } = require("selenium-webdriver");
 async function drag() {
-  let driver = await new Builder().forBrowser('chrome').build()
+  let driver = await new Builder().forBrowser("chrome").build();
   try {
-    await driver.get('http://localhost/drag/main.html')
+    await driver.get("http://localhost/drag/main.html");
 
+    // Not Working in action code
+    // let draggableDOM = await driver.findElement(By.id("draggable"));
+    // let droppableDOM = await driver.findElement(By.id("droppable"));
+
+    // await driver
+    //   .actions()
+    //   .dragAndDrop(draggableDOM, droppableDOM)
+    //   .perform()
+    //   .then(result => {
+    //     console.log("Executed Successfully");
+    //   })
+    //   .catch(err => {
+    //     console.log("Failed to perform action");
+    //   });
+
+    // Working by using similation script
     let myscript = `function trigger_drop() {
       var draggable = $("#draggable").draggable(),
         droppable = $("#droppable").droppable(),
@@ -19,17 +35,17 @@ async function drag() {
       });
     }
     trigger_drop()
-    
-    `
+
+    `;
     await driver
       .executeScript(myscript)
-      .then(res => console.log('script executed'))
-      .catch(err => console.log('err', err))
+      .then(res => console.log("Script executed successfully", res))
+      .catch(err => console.log("Failed to perform action", err));
 
     //
   } finally {
-    //await driver.quit()
+    await driver.quit();
   }
 }
 
-drag()
+drag();
